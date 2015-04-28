9d5120de34ad3f28f80e342d5d45edfd965b36f6
Tom Bartoň <grez.cz@gmail.com>
2015-04-29 00:53:22 +0200

    User Log model

--- app/model/UserLog.php
+++ app/model/UserLog.php
@@ -26,13 +26,19 @@ class UserLog extends \Kdyby\Doctrine\Entities\BaseEntity
 
     /**
      * @ORM\Column(type="integer")
+     * 0 - 1000 reserved for Teddy
      */
     protected $action = 0;
 
     /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $type = 0;
+
+    /**
      * @ORM\Column(type="string")
      */
-    protected $description = '';
+    protected $data;
 
     /**
      * @ORM\Column(type="datetime")
@@ -41,9 +47,88 @@ class UserLog extends \Kdyby\Doctrine\Entities\BaseEntity
     protected $date;
 
 
+    /** Action types */
+    const ADMIN = 1;
+    const USER = 2;
+    const GAME = 3;
+
+
+    /** Admin actions */
+    const ADMIN_DAEMON = 1;
+    const ADMIN_EDIT_USER = 2;
+    const ADMIN_DELETE_USER = 3;
+    const ADMIN_BAN_USER = 4;
+    const ADMIN_CREATE_ADMIN = 5;
+    const ADMIN_EDIT_ADMIN = 6;
+    const ADMIN_DELETE_ADMIN = 7;
+
+    /** User actions */
+    const USER_CHANGE_PASSWORD = 1;
+    const USER_DELETE_USER = 2;
+
+    /** Game actions */
+
+
     public function __construct()
     {
         $this->date = new \DateTime();
+    }
+
+    /**
+     * Internally stores data serialized
+     * @param mixed $data
+     * @return null
+     */
+    public function setData($data)
+    {
+        $this->data = serialize($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return unserialize($this->data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return sprintf($this->getTemplate(), $this->getData());
+    }
+
+    /**
+     * @return string
+     * @throws \TeddyException
+     */
+    protected function getTemplate()
+    {
+        $templates = array(
+            self::ADMIN => array(
+                self::ADMIN_DAEMON => 'Daemon %s',
+                self::ADMIN_EDIT_USER => 'Edited user %s',
+                self::ADMIN_DELETE_USER => 'Deleted user %s',
+                self::ADMIN_BAN_USER => 'Banned user %s',
+                self::ADMIN_CREATE_ADMIN => 'Created admin %s',
+                self::ADMIN_EDIT_ADMIN => 'Edited admin %s',
+                self::ADMIN_DELETE_ADMIN => 'Deleted admin %s',
+            ),
+            self::USER => array(
+                self::USER_CHANGE_PASSWORD => 'Changed password',
+                self::USER_DELETE_USER => 'Deleted profile',
+            ),
+            self::GAME => array(
+            ),
+        );
+
+        if (isset($templates[$this->type][$this->action])) {
+            return $templates[$this->type][$this->action];
+        }
+
+        throw new \TeddyException('Unknown action ' . $this->type . ':' . $this->action);
     }
 
 }
--- app/model/UserLogs.php
+++ app/model/UserLogs.php
@@ -7,4 +7,22 @@ use Nette;
 class UserLogs extends Manager
 {
 
+    public function log(User $user, $type, $action, $data = null)
+    {
+        $userLog = new UserLog;
+        $userLog->setUser($user);
+        $userLog->setType($type);
+        $userLog->setAction($action);
+        $userLog->setData($data);
+        $this->save($userLog);
+    }
+
+    public function getLogs($type)
+    {
+        $criteria = array(
+            'type' => $type,
+        );
+        return $this->findBy($criteria);
+    }
+
 }
