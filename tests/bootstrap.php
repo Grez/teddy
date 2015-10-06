14fa2434e9f7a5553a14ef52acd11131ef31f902
Tom Barton <grez.cz@gmail.com>
2015-10-06 19:55:10 +0200

    Bugfix, tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,13 +9,10 @@ if (!class_exists('Tester\Assert')) {
 
 Tester\Environment::setup();
 
-$configurator = new Nette\Configurator;
-$configurator->setDebugMode(FALSE);
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$parameters = ['wwwDir' => __DIR__ . '/../www'];
+$configurator = new Teddy\Configurator($parameters);
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__ . '/../app')
 	->register();
 
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
-return $configurator->createContainer();
+$container = $configurator->createContainer();
