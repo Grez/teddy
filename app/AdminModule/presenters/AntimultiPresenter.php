acfbd12b2469718d77b3330fa5c15a75d6d82501
Tom Barton <grez.cz@gmail.com>
2015-10-26 01:56:45 +0100

    Added antimulti admin presenter

--- app/AdminModule/presenters/AntimultiPresenter.php
+++ app/AdminModule/presenters/AntimultiPresenter.php
@@ -2,10 +2,107 @@
 
 namespace Teddy\AdminModule\Presenters;
 
+use Nette\Utils\ArrayHash;
+use Teddy\Entities\User\Login;
+use Teddy\Entities\User\LoginListQuery;
+use Teddy\Entities\User\UserAgent;
+use Teddy\Forms\Form;
+
+
+
 /**
  * @TODO: kontrolor, zahraničí, thor hammer
  */
 class AntimultiPresenter extends BasePresenter
 {
+
+	/**
+	 * Shows list of new users, email, similar passwords?, user agent, IP
+	 */
+	public function renderNewProfiles()
+	{
+
+	}
+
+
+
+	public function renderDefault($type, $success, $text)
+	{
+		$query = (new LoginListQuery());
+
+		if ($success) {
+			$this['loginForm']['success']->setDefaultValue($success);
+			if ($success === 'success') {
+				$query->onlySuccessful();
+			}
+
+			if ($success === 'success') {
+				$query->onlyUnsuccessful();
+			}
+		}
+
+		if ($type) {
+			$this['loginForm']['type']->setDefaultValue($type);
+		}
+
+
+		if ($text) {
+			$this['loginForm']['text']->setDefaultValue($text);
+			switch ($type) {
+				case 'user':
+					$user = $this->users->getByNick($text);
+					$query->byUser($user);
+					break;
+				case 'userAgent':
+					$userAgent = $this->em->getRepository(UserAgent::class)->findOneBy(['userAgent' => $text]);
+					$query->byUserAgent($userAgent);
+					break;
+				case 'ip':
+					$query->byIp($text);
+					break;
+				case 'cookie':
+					$query->byCookie((int) $text);
+					break;
+			}
+		}
+
+		$this->template->logins = $this->em->getRepository(Login::class)
+			->fetch($query);
+	}
+
+
+
+	protected function createComponentLoginForm()
+	{
+		$form = new Form();
+		$form->addSelect('type', 'Type', [
+			'user' => 'User',
+			'ip' => 'IP',
+			'cookie' => 'Cookie',
+			'userAgent' => 'User agent',
+			'fingerprint' => 'Fingerprint hash',
+		]);
+		$form->addSelect('success', 'Success', [
+			'all' => 'All attempts',
+			'success' => 'Only successful',
+			'failed' => 'Only failed',
+		]);
+		$form->addText('text', 'Text');
+		$form->addSubmit('submit', 'Submit');
+		$form->onSuccess[] = function (Form $form, ArrayHash $values) {
+			$this->redirect('this', ['type' => $values->type, 'success' => $values->success, 'text' => $values->text]);
+		};
+		return $form->setBootstrapRenderer();
+	}
+
+
+
+	/**
+	 * @return \VisualPaginator
+	 */
+	protected function createComponentVp()
+	{
+		return new \VisualPaginator();
+	}
 
 }
---
+++ app/entities/User/Queries/LoginListQuery.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Teddy\Entities\User;
+
+use Doctrine\ORM\QueryBuilder;
+use Kdyby\Persistence\Queryable;
+
+
+
+class LoginListQuery extends \Kdyby\Doctrine\QueryObject
+{
+
+	/**
+	 * @var array|\Closure[]
+	 */
+	private $filter = [];
+
+	/**
+	 * @var array|\Closure[]
+	 */
+	private $select = [];
+
+
+
+	/**
+	 * @param User $user
+	 * @return $this
+	 */
+	public function byUser(User $user = NULL)
+	{
+		$this->filter[] = function (QueryBuilder $qb) use ($user) {
+			$qb->andWhere('l.user = :user')->setParameter('user', $user);
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @param UserAgent $userAgent
+	 * @return $this
+	 */
+	public function byUserAgent(UserAgent $userAgent = NULL)
+	{
+		$this->filter[] = function (QueryBuilder $qb) use ($userAgent) {
+			$qb->andWhere('l.userAgent = :userAgent')->setParameter('userAgent', $userAgent);
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string $ip
+	 * @return $this
+	 */
+	public function byIp($ip)
+	{
+		$this->filter[] = function (QueryBuilder $qb) use ($ip) {
+			$qb->andWhere('l.ip = :ip')->setParameter('ip', $ip);
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @param integer $cookie
+	 * @return $this
+	 */
+	public function byCookie($cookie)
+	{
+		$this->filter[] = function (QueryBuilder $qb) use ($cookie) {
+			$qb->andWhere('l.cookie = :cookie')->setParameter('cookie', $cookie);
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @param $fingerprint
+	 * @return $this
+	 */
+	public function byFingerprint($fingerprint)
+	{
+		$this->filter[] = function (QueryBuilder $qb) use ($fingerprint) {
+			$qb->andWhere('l.fingerprint = :fingerprint')->setParameter('fingerprint', $fingerprint);
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @return $this
+	 */
+	public function onlySuccessful()
+	{
+		$this->filter[] = function (QueryBuilder $qb) {
+			$qb->andWhere('l.error = 0');
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @return $this
+	 */
+	public function onlyUnsuccessful()
+	{
+		$this->filter[] = function (QueryBuilder $qb) {
+			$qb->andWhere('l.error > 0');
+		};
+		return $this;
+	}
+
+
+
+	/**
+	 * @param Queryable $repository
+	 * @return \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder
+	 */
+	protected function doCreateQuery(Queryable $repository)
+	{
+		$qb = $this->createBasicDql($repository);
+
+		foreach ($this->select as $modifier) {
+			$modifier($qb);
+		}
+
+		return $qb;
+	}
+
+
+
+	/**
+	 * @param Queryable $repository
+	 * @return \Kdyby\Doctrine\QueryBuilder
+	 */
+	protected function doCreateCountQuery(Queryable $repository)
+	{
+		return $this->createBasicDql($repository)->select('COUNT(l.id)');
+	}
+
+
+
+	/**
+	 * @param Queryable $repository
+	 * @return \Kdyby\Doctrine\QueryBuilder
+	 */
+	private function createBasicDql(Queryable $repository)
+	{
+		$qb = $repository->createQueryBuilder()
+			->select('l')
+			->from(Login::class, 'l')
+			->innerJoin(User::class, 'u')
+			->setMaxResults(500)
+			->addOrderBy('l.date', 'DESC');
+
+		foreach ($this->filter as $modifier) {
+			$modifier($qb);
+		}
+
+		return $qb;
+	}
+
+}
