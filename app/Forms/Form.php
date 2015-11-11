c0ea66c069bb8ebaa6ae20019e37dfb118b8e48b
Tom Barton <grez.cz@gmail.com>
2015-11-11 23:52:05 +0100

    Added method PropagateErrors to Form

--- app/Forms/Form.php
+++ app/Forms/Form.php
@@ -5,6 +5,7 @@ namespace Teddy\Forms;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\PersistentCollection;
 use Kdyby\Doctrine\Entities\BaseEntity;
+use Nette\Application\UI\Control;
 use Nette\Forms\Container;
 use Nette;
 use Nette\Forms\Controls;
@@ -20,6 +21,69 @@ class Form extends \Nette\Application\UI\Form
 
 	/** @var BaseEntity */
 	protected $entity;
+
+
+
+	/**
+	 * Sets Bootstrap 3 rendering
+	 * @warning needs to be called after elements are created (otherwise classes for inputs aren't added)
+	 * @return self
+	 */
+	public function setBootstrapRenderer()
+	{
+		/** @var Nette\Forms\Rendering\DefaultFormRenderer $renderer */
+		$renderer = parent::getRenderer();
+		$renderer->wrappers['controls']['container'] = NULL;
+		$renderer->wrappers['pair']['container'] = 'div class=form-group';
+		$renderer->wrappers['pair']['.error'] = 'has-error';
+		$renderer->wrappers['control']['container'] = 'div class=col-sm-9';
+		$renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
+		$renderer->wrappers['control']['description'] = 'span class=help-block';
+		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+
+		// make form and controls compatible with Twitter Bootstrap
+		$this->getElementPrototype()->class('form-horizontal');
+
+		foreach ($this->getControls() as $control) {
+			if ($control instanceof Controls\Button) {
+				$control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
+				$usedPrimary = TRUE;
+			} elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox) {
+				$control->getControlPrototype()->addClass('form-control');
+			} elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList) {
+				if ($control->getSeparatorPrototype()->getName() == 'inline') {
+					$control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type . '-inline');
+				} else {
+					$control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
+				}
+			}
+		}
+
+		return $this;
+	}
+
+
+
+	/**
+	 * Propagates form errors to $control or Presenter as flashMessage of type 'error'
+	 *
+	 * @param Control|NULL $control
+	 */
+	public function propagateErrors(Control $control = NULL)
+	{
+		$this->onError[] = function () use ($control) {
+			/** @var Control $control */
+			$control = $control ?: $this->getPresenter();
+
+			foreach ($this->getErrors() as $error) {
+				if (empty($error)) {
+					continue;
+				}
+
+				$control->flashMessage($error, 'error');
+			}
+		};
+	}
 
 
 
@@ -68,46 +132,6 @@ class Form extends \Nette\Application\UI\Form
 				$input->setDefaultValue($value);
 			}
 		}
-	}
-
-
-
-	/**
-	 * Sets Bootstrap 3 rendering
-	 * @warning needs to be called after elements are created (otherwise classes for inputs aren't added)
-	 * @return self
-	 */
-	public function setBootstrapRenderer()
-	{
-		/** @var Nette\Forms\Rendering\DefaultFormRenderer $renderer */
-		$renderer = parent::getRenderer();
-		$renderer->wrappers['controls']['container'] = NULL;
-		$renderer->wrappers['pair']['container'] = 'div class=form-group';
-		$renderer->wrappers['pair']['.error'] = 'has-error';
-		$renderer->wrappers['control']['container'] = 'div class=col-sm-9';
-		$renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
-		$renderer->wrappers['control']['description'] = 'span class=help-block';
-		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
-
-		// make form and controls compatible with Twitter Bootstrap
-		$this->getElementPrototype()->class('form-horizontal');
-
-		foreach ($this->getControls() as $control) {
-			if ($control instanceof Controls\Button) {
-				$control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
-				$usedPrimary = TRUE;
-			} elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox) {
-				$control->getControlPrototype()->addClass('form-control');
-			} elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList) {
-				if ($control->getSeparatorPrototype()->getName() == 'inline') {
-					$control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type . '-inline');
-				} else {
-					$control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
-				}
-			}
-		}
-
-		return $this;
 	}
 
 
