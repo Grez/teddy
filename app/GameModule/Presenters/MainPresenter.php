1e2de7383edef67c73d86e1e1ef7aa338d58c51a
Tom Barton <grez.cz@gmail.com>
2016-04-03 18:05:42 +0200

    Dropped map (switched to its own package)

--- app/GameModule/Presenters/MainPresenter.php
+++ app/GameModule/Presenters/MainPresenter.php
@@ -11,19 +11,4 @@ use Teddy\GameModule\Components\IMapControlFactory;
 class MainPresenter extends BasePresenter
 {
 
-	/**
-	 * @var IMapControlFactory
-	 * @inject
-	 */
-	public $mapControlFactory;
-
-
-
-	protected function createComponentMap()
-	{
-		$map = $this->em->find(Map::class, 56);
-		$startPosition = $this->em->find(Position::class, '56;-37;-34');
-		return $this->mapControlFactory->create($map, $startPosition);
-	}
-
 }
