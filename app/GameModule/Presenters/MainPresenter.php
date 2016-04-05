e2c3a56ef98c3fa981be119d1a43ef17906e6070
Tom Barton <grez.cz@gmail.com>
2016-04-05 19:22:21 +0200

    Unused uses

--- app/GameModule/Presenters/MainPresenter.php
+++ app/GameModule/Presenters/MainPresenter.php
@@ -2,10 +2,6 @@
 
 namespace Teddy\GameModule\Presenters;
 
-use Teddy\Entities\Map\Map;
-use Teddy\Entities\Map\Position;
-use Teddy\GameModule\Components\IMapControlFactory;
-
 
 
 class MainPresenter extends BasePresenter
